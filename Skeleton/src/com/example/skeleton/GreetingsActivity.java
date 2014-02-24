32611bb66dfeb4187353d3071d5625386d94cad8
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-02-23 22:46:21 -0500

    async task created

--- Skeleton/src/com/example/skeleton/GreetingsActivity.java
+++ Skeleton/src/com/example/skeleton/GreetingsActivity.java
@@ -1,15 +1,38 @@
 package com.example.skeleton;
 
+
+import java.util.ArrayList;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.example.skeleton.models.Greeting;
+import com.example.skeleton.models.GreetingList;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GreetingsActivity extends Activity {
+	private final String apiBaseUrl = "http://localhost:49811/";
+	private final String apiRefreshUrl = apiBaseUrl + "api/greetings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greetings);
+        final Button btnRefresh= (Button) findViewById(R.id.btnRefresh);
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+    	    @Override
+    	    public void onClick(View v) {
+    	    	new AsyncRefreshGreetings().execute();
+    	    }
+    	});
     }
 
 
@@ -18,6 +41,42 @@ public class GreetingsActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.greetings, menu);
         return true;
+    }
+    
+    private class AsyncRefreshGreetings extends AsyncTask<Void, Void, ArrayList<Greeting>> {
+        @Override
+        protected ArrayList<Greeting> doInBackground(Void... params) {
+        	try{
+    	    	RestTemplate restTemplate = new RestTemplate();
+    	        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+    	        @SuppressWarnings("unchecked")
+    			ArrayList<Greeting> greetings = restTemplate.getForObject(apiRefreshUrl, ArrayList.class);
+    	        return greetings;
+        	}catch(Exception e){
+        		Toast.makeText(getApplicationContext(), "Error: " + e.getStackTrace(), Toast.LENGTH_LONG).show();
+        	}
+
+            return new ArrayList<Greeting>();
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Greeting> greetings) {            
+        	final TextView textViewGreetingsResults = (TextView) findViewById(R.id.textViewGreetingsResults);
+	        if(greetings == null || greetings.size() == 0){
+	        	textViewGreetingsResults.setText("[None]");
+	        }
+	        else{
+	        	StringBuilder sb = new StringBuilder();
+	        	for(Greeting g : greetings){
+	        		sb.append(g.getId());
+	        		sb.append(": ");
+	        		sb.append(g.getContent());
+	        		sb.append("\n");
+	        	}
+	        	textViewGreetingsResults.setText(sb.toString());
+	        }
+        }
+
     }
     
 }
---
+++ Skeleton/src/com/example/skeleton/models/GreetingList.java
@@ -0,0 +1,7 @@
+package com.example.skeleton.models;
+
+import java.util.ArrayList;
+
+public class GreetingList extends ArrayList<Greeting> {
+	private static final long serialVersionUID = 7123317409723821313L;
+}
